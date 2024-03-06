d03cff0dfe73d869e7c2a5d253baaa579cdb10da
Yeltsin <ychuquilinc@unc.edu.pe>
2024-03-05 23:12:20 -0500

    calculadora

---
+++ Py_Sci_Calculator.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import math
+import parser
+import tkinter.messagebox
+
+root = Tk()
+root.title("Calculadora Cientifica")
+root.resizable(width=False, height=False)
+root.geometry("400x492+460+40")
+
+
+MainFrame = Frame(root                  )
+
+
+
