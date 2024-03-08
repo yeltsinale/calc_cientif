1e455b71c765cb6909eafdcd126f9547a56c1438
Yeltsin <ychuquilinc@unc.edu.pe>
2024-03-07 23:26:40 -0500

    calculadora

--- Py_Sci_Calculator.py
+++ Py_Sci_Calculator.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import math
-import parser
 import tkinter.messagebox
 
 root = Tk()
@@ -8,11 +7,21 @@ root.title("Calculadora Cientifica")
 root.resizable(width=False, height=False)
 root.geometry("400x492+460+40")
 
-
+#frame -> marco contenedor
 MainFrame = Frame(root, bd=20, pady = 2 , relief = RIDGE).grid()
-calc = Frame(MainFrame, bd=20, pady = 2 , relief = RIDGE).grid()
+#grid rejilla
+
+calc = Frame(MainFrame, bd=20, pady = 2 , relief = RIDGE)
+calc.grid()
+
+menubar = Menu(calc)
+#tearoff -> abre otra ventan
+filemenu = Menu(menubar, tearoff= 0)
+menubar.add_cascade(label = "File", menu =filemenu)
+filemenu.add_command(label = "Standard", menu =filemenu)
 
 
+root.mainloop()
 
 
 
