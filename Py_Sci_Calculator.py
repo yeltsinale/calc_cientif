730b8c6da134c5b301b8fd16ae4b4dd0111ec26c
Yeltsin <ychuquilinc@unc.edu.pe>
2024-03-13 23:44:36 -0500

    calculadora

--- Py_Sci_Calculator.py
+++ Py_Sci_Calculator.py
@@ -6,10 +6,7 @@ root = Tk()
 root.title("Calculadora Científica")
 root.resizable(width=False, height=False)
 root.geometry("400x492+460+40")
-##################
-######
 
-print("HI")
 
 #frame -> marco contenedor
 MainFrame = Frame(root, bd=20, pady = 2 , relief = RIDGE).grid()
@@ -18,14 +15,22 @@ MainFrame = Frame(root, bd=20, pady = 2 , relief = RIDGE).grid()
 calc = Frame(MainFrame, bd=20, pady = 2 , relief = RIDGE)
 calc.grid()
 
+
+class Calc():
+    def __init__(self):
+        self.total = 0
+
+
 menubar = Menu(calc)
 #tearoff -> abre otra ventan
 filemenu = Menu(menubar, tearoff= 0)
 menubar.add_cascade(label = "Archivo", menu =filemenu)
 filemenu.add_command(label = "Standard")
 filemenu.add_separator()
-filemenu.add_command(label = "Científica")
+filemenu.add_command(label = "Cientifica")
+filemenu.add_command(label = "xD    ")
 filemenu.add_separator()
+filemenu.add_command(label = "Salir")
 filemenu.add_command(label = "Salir")
 
 root.config(menu = menubar)
